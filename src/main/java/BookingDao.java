3636c9f08654850b211eba0c9531b5003f520cdb
Uguraz <114384145+Uguraz@users.noreply.github.com>
2023-05-01 12:00:00 +0200

    BookingDao og Impl + DatabaseConnector

---
+++ src/main/java/BookingDao.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+import java.time.LocalDateTime;
+
+
+public interface BookingDao {
+
+
+        public void saveBooking(int bookingID, int packageID, int activityID, int organizationID, String firstName, String lastName, String position, String phone, String email, String åbenSkoleForløb, String transportType, String transportArrival, String transportDeparture, int participants, LocalDateTime startDate, LocalDateTime endDate, LocalDateTime bookingDate, boolean temporaryBooking, String assistance, boolean noShow, String messageToAS, String personalNote);
+
+
+        public void deleteBooking(Booking booking);
+    }
+
+
