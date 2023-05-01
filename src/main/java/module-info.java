472b89a4e84b7a698ee1c3b6726e7c44523c2cd8
Uguraz <114384145+Uguraz@users.noreply.github.com>
2023-05-01 11:40:56 +0200

    BookingDao og Impl + DatabaseConnector

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module group3.mindfactory_booking {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
 
 
     opens group3.mindfactory_booking to javafx.fxml;
