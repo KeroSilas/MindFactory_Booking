6a7ba72b02c876b62c3fa7dc86a2424427e6d32f
chrsut01 <chrissu360@gmail.com>
2023-05-02 12:05:42 +0200

    amend

--- src/main/java/group3/mindfactory_booking/model/Forløb.java
+++ src/main/java/group3/mindfactory_booking/model/Forløb.java
@@ -1,4 +1,28 @@
 package group3.mindfactory_booking.model;
 
 public class Forløb {
+
+    private int forløbID;
+    private String forløbName;
+
+    public Forløb(int forløbID, String forløbName) {
+        this.forløbID = forløbID;
+        this.forløbName = forløbName;
+    }
+
+    public int getForløbID() {
+        return forløbID;
+    }
+
+    public void setForløbID(int forløbID) {
+        this.forløbID = forløbID;
+    }
+
+    public String getForløbName() {
+        return forløbName;
+    }
+
+    public void setForløbName(String forløbName) {
+        this.forløbName = forløbName;
+    }
 }
