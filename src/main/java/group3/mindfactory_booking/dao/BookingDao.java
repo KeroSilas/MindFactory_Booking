aec1b9407f7947b6a447b06da47cf54cb8fc3555
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-05-04 16:48:35 +0200

    amend

--- src/main/java/group3/mindfactory_booking/dao/BookingDao.java
+++ src/main/java/group3/mindfactory_booking/dao/BookingDao.java
@@ -2,14 +2,16 @@ package group3.mindfactory_booking.dao;
 
 import group3.mindfactory_booking.model.singleton.Booking;
 
+import java.util.List;
+
 
 public interface BookingDao {
 
+    public void saveBooking(Booking booking);
 
-        public void saveBooking(Booking booking);
+    List<Integer> getAllBookingID();
 
-
-        public void deleteBooking(int bookingID);
+    public void deleteBooking(int bookingID);
     }
 
 
