8875f1a78357b70c0e7c68af1c240482d0f4f71c
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-05-08 16:22:03 +0200

    Changes

--- src/main/java/group3/mindfactory_booking/model/tasks/SendReminderEmailTask.java
+++ src/main/java/group3/mindfactory_booking/model/tasks/SendReminderEmailTask.java
@@ -23,7 +23,7 @@ public class SendReminderEmailTask extends Task<Void> {
     protected Void call() {
         while (!isCancelled()) { // Keep running until the task is cancelled
             try {
-                Thread.sleep(30000L); // Sleep for the specified delay
+                Thread.sleep(30000L);
             } catch (InterruptedException e) {
                 if (isCancelled()) {
                     break;
