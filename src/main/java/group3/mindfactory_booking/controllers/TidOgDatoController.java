7899e54f6bf50216bf81f0d384c29124850a840f
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-05-10 10:00:14 +0200

    Changes

--- src/main/java/group3/mindfactory_booking/controllers/TidOgDatoController.java
+++ src/main/java/group3/mindfactory_booking/controllers/TidOgDatoController.java
@@ -136,6 +136,7 @@ public class TidOgDatoController {
                             }
                         }
                     }
+                    sameDayCounter = 0;
                 }
             }
         });
